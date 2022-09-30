bc84b8ed4088d2a483d78a38d27e1887b894e12d
rekayajabar <113187163+rekayajabar@users.noreply.github.com>
2022-09-30 16:25:03 -0500

    Update lab 5..py

--- lab 5..py
+++ lab 5..py
@@ -6,7 +6,8 @@ Email: rjznp@umsystem.edu
 
 PROBLEM : Describe the problem
 
- ALGORITHM :      1. Write out the algorithm
+ ALGORITHM : 
+      Write out the algorithm
  
  ERROR HANDLING:
       Any Special Error handling to be noted.  Wager not less than 0. etc
